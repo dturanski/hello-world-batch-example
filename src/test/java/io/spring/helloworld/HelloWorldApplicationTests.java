3309722f209d0ab7eb8fd157ea01b0eae4544ae1
David Turanski <dturanski@pivotal.io>
2018-03-09 14:04:19 -0500

    Upgrade to spring-cloud-task-starter 2.0.0.BUILD-SNAPSHOT

--- src/test/java/io/spring/helloworld/HelloWorldApplicationTests.java
+++ src/test/java/io/spring/helloworld/HelloWorldApplicationTests.java
@@ -14,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
